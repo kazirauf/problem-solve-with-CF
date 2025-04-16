446e88ce4f3165b1062cfa9758980935ef0ece57
kazi rauf <raufelahi9876@gmail.com>
2025-04-16 12:11:27 +0600

    even number problem is done

--- MaxandMin.c
+++ MaxandMin.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
 
 int main (){
-  
+   int a, b, c;
+   scanf("%d %d %d ", &a, &b, &c);
+
+
+    // Find the minimum number
+   if(a <= b && a <= c){
+    printf("%d ", a);
+
+   }
+   else if(b <= a && b <= c){
+    printf("%d ", b);
+   }
+   else if(c <= a && c <= b){
+     printf("%d ", c);
+     
+   }
+
+    // Find the maximum number
+   if(a >= b && a >= c){
+    printf("%d", a);
+
+   }
+   else if(b >= a && b >= c){
+    printf("%d", b);
+   }
+   else if(c >= a && c >= b){
+     printf("%d", c);
+
+   }
+
     return 0;
 }
